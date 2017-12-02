0a3a0c3a1fc0226369b19972910ffa110a733162
saddam1997 <saddam.husen1997@gmail.com>
2017-12-02 17:47:48 +0530

    remove git ignore file CFGasdf

---
+++ frontend/cfg/cfg.php
@@ -0,0 +1,8 @@
+<?php
+
+class object {}
+$CFG = new object ( );
+$CFG->api_url = 'http://localhost/wlox/api/htdocs/api.php';
+$CFG->auth_login_url = 'http://localhost/wlox/auth/htdocs/login.php';
+$CFG->auth_verify_token_url = 'http://localhost/wlox/auth/htdocs/verify_token.php';
+?>
